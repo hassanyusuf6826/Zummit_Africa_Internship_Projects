853b66bd1a478d9d9122c8f6a55ef6db9ee27fab
hassanyusuf6826 <160252819+hassanyusuf6826@users.noreply.github.com>
2025-02-18 06:10:07 +0100

    Update fashion_app.py

--- Fashion-Syst/fashion_app.py
+++ Fashion-Syst/fashion_app.py
@@ -13,7 +13,7 @@ import numpy as np
 import glob
 from zipfile import ZipFile
 from PIL import Image
-from tensorflow.keras.preprocessing import image
+from keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.models import Model
 from scipy.spatial.distance import cosine
